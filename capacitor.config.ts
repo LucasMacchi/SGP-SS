e7fba1e4916de28bf00d6858fa0d83e3da7a3cc2
Lucas Macchi <lucasmacchi25@gmail.com>
2025-04-04 14:22:11 -0300

    d

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ss.sgpm',
+  appName: 'sgpm',
+  webDir: 'dist'
+};
+
+export default config;
