cf075b8538671855315cf9907569b80ce2ff9991
Lucas Macchi <lucasmacchi25@gmail.com>
2025-04-04 15:33:13 -0300

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
+const manifestIcons = [
+  {
+    src: '/logo_small.webp',
+    sizes: '192x192',
+    type: 'image/png',
+  }
+]
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Sistema Gestor de Pedidos',
+        short_name: 'SGP app',
+        icons: manifestIcons,
+      }
+    })
+  ],
 })
