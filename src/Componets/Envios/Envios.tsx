2e3d63d9e62eebb85351a498839c9c81f04cb7a1
LucasMacchi <lucasmacchi25@gmail.com>
2025-09-19 08:59:25 -0300

    delete tanda

--- src/Componets/Envios/Envios.tsx
+++ src/Componets/Envios/Envios.tsx
@@ -1,13 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import Header from "../Header/Header";
-import { IDesglose, IEnvio, IInsumoRac, ILgarEntrega, IpedidoRacDataPDF, IPedidoRacPDF } from "../../Utils/Interfaces";
+import { IDesglose, IInsumoRac, ILgarEntrega, IpedidoRacDataPDF, IPedidoRacPDF } from "../../Utils/Interfaces";
 import lentregaService from "../../Utils/lentregaService";
 import { GlobalContext } from "../../Context/GlobalContext";
 import PedidoRacPdf from "../pdfs/pedidoRac";
 import saveAs from "file-saver";
 import { pdf } from "@react-pdf/renderer";
 import insRacionamieto from "./insumos.json"
-import desgloseReturner from "../../Utils/desgloseReturner";
 import DesglosePdf from "../pdfs/desgloses";
 import createTxtEnvio from "../../Utils/createTxtEnvio";
 import RutaPdf from "../pdfs/rutaEnvioPdf";
@@ -22,12 +21,8 @@ export default function Envios () {
     const [desglosesF, setDesglosesF] = useState<IDesglose[]>([])
     const [desglosesS, setDesglosesS] = useState("")
     const [lgaresS, setLgaresS] = useState("")
-    const [lgarSel, setLgarSel] = useState(0)
-    const [desSel, setDesSel] = useState(0)
-    const [newEnvios, setNewEnvios] = useState<IEnvio[]>([])
-    const [displayDes, setDisplayDes] = useState(false)
-    const [updater, setUpdater] = useState(0)
     const [tanda, setTanda] = useState(0)
+    const [delkey, setDelkey] = useState("")
     const [dias, setDias] = useState(0)
     const [insRac, setInsRac] = useState<IInsumoRac>({
         des: "",kg: 0,cajas: 0,
@@ -55,6 +50,8 @@ export default function Envios () {
         setInsRac({des: "",kg: 0,cajas: 0,bolsas: 0,rac: 0,sel: 0})
         setLgaresS("")
         setDesglosesS("")
+        setDelkey("")
+        setTanda(0)
     },[display])
 
     useEffect(() => {
@@ -85,11 +82,6 @@ export default function Envios () {
             setInsRac({...insRac ,rac: raciones, kg: parseFloat(kilos)})
         }
     },[insRac.bolsas, insRac.cajas])
-
-    useEffect(() => {
-        if(lgarSel > 0) setDisplayDes(true)
-        else setDisplayDes(false)
-    },[lgarSel])
 
 
     const displayDesglosadoGn = () => {
@@ -240,212 +232,39 @@ export default function Envios () {
         )
     }
 
-    const displayNewEnvio = () => {
-        const checkEnvios = () : boolean => {
-            let res = true
-            newEnvios.map((en) => {
-                if(en.productos.length === 0) res = false
-            })
-            console.log(res)
-            return res
-        }
-        const addEnvio = () => {
-            if(desSel) {
-                const newEnv:IEnvio = {
-                    lentregaId: lgarSel,
-                    desgloseId: desSel,
-                    userId: 0,
-                    nro: "",
-                    identificador: "",
-                    estado: "",
-                    productos:[],
-                    addprod: {des: "",kg: 0,cajas: 0,bolsas: 0,rac: 0,sel: 0}
-                }
-                newEnvios.push(newEnv)
-                setDesSel(0)
-            } else alert("")
-        }
-        const deleteEnvio = (index: number) => {
-            if(confirm("Quieres eliminar el envio?")) {
-                const arr = newEnvios.filter((_en,i) => i !== index)
-                newEnvios.splice(index, 1)
-                setNewEnvios(arr)
-            }
-        }
-        const deleteProducto = (indexE: number, index: number) => {
-            if(confirm("Quieres eliminar el producto?")){
-                newEnvios[indexE].productos.splice(index,1)
-                const up = updater + 1
-                setUpdater(up)
-            }
-        }
-        const clearSelection = () => {
-            if(confirm("Quieres seleccionar devuelta el lugar de entrega? Esto limpiara todo el envio.")) {
-                setNewEnvios([])
-                setLgarSel(0)
-            }
-        }
-        const handlerCaja = (index: number, payload: number) => {
-            const modEnv = newEnvios[index]
-            modEnv.addprod.cajas = payload
-            newEnvios[index] = modEnv
-            setNewEnvios(newEnvios)
-            const up = updater + 1
-            setUpdater(up)
-        }
-        const handlerBolsa = (index: number, payload: number) => {
-            const modEnv = newEnvios[index]
-            modEnv.addprod.bolsas = payload
-            newEnvios[index] = modEnv
-            setNewEnvios(newEnvios)
-            const up = updater + 1
-            setUpdater(up)
-        }
-        const handlerSel = (index: number, payload: number) => {
-            const modEnv = newEnvios[index]
-            modEnv.addprod.sel = payload
-            newEnvios[index] = modEnv
-            setNewEnvios(newEnvios)
-            const up = updater + 1
-            setUpdater(up)
-        }
-        const addInsumo = (i: number) => {
-            const insumo = newEnvios[i].addprod
-            if(insumo.sel && (insumo.bolsas || insumo.cajas)) {
-                const racion = insRacionamieto.insumos[insumo.sel]
-                const raciones = racionesCalc(insumo.cajas, insumo.bolsas, racion.c,racion.b)
-                const kilos = (racionesKilosCalc(raciones,racion.w, racion.b) / 1000).toFixed(2)
-                const data: IInsumoRac = {
-                    bolsas: insumo.bolsas,
-                    cajas: insumo.cajas,
-                    rac: raciones,
-                    kg: parseFloat(kilos),
-                    des: racion.des
-                }
-                newEnvios[i].productos.push(data)
-                handlerBolsa(i,0)
-                handlerCaja(i,0),
-                handlerSel(i,0)
-                const up = updater + 1
-                setUpdater(up)
-            }
+    const displayDeleteTanda = () => {
 
+        const deleteEnvio = async () => {
+            await global?.deleteTandaFn(tanda, delkey)
+            setDelkey("")
+            setTanda(0)
         }
-        const createEnvios = async () => {
-            if(checkEnvios()) {
-                global?.registerEnvio(newEnvios)
-                //setNewEnvios([])
-                //setLgarSel(0)
-            }
-            else alert("Asegurese que todos los envios tengan insumos.")
-        }
-        return(
+        return (
             <div>
                 <hr color='#3399ff' className='hr-line'/>
                 <div>
                     <h2 className='title-Homepage' >
-                        Generar Envio 
+                        Eliminar Envio 
                     </h2>
                     <div>
-                        <h4 className='title-Homepage'>Seleccione la cabecera - encontrados - {lgaresF.length}</h4>
-                        <div style={{ display: "flex",flexDirection: "row"}}>
-                            <h4 className='title-Homepage'>Buscar: </h4>
-                            <input type="text" id='otherins' className="data-div-select" value={lgaresS}
-                            style={{width: "58%"}} onChange={(e) => setLgaresS(e.target.value)}/>
-                        </div>
-                        <div>
-                            <select defaultValue={0}
-                            value={lgarSel} disabled={lgarSel > 0 ? true : false} onChange={(e) => setLgarSel(parseInt(e.target.value))} className='select-small-cco'>
-                                <option value={0}>---</option>
-                                {
-                                    lgaresF.map((lg,i) => (<option key={i} value={lg.lentrega_id}>{lg.lentrega_id+"-"+lg.descripcion}</option>))
-                                }
-                            </select>
-                            <button className='btn-export-pdf' onClick={() => clearSelection()}>Limpiar</button>
-                        </div>
-                        <div>
-                            {displayDes && 
-                            <div>
-                                <h4 className='title-Homepage'>Desgloses encontrados - {desglosesF.length}</h4>
-                                <div style={{ display: "flex",flexDirection: "row"}}>
-                                    <h4 className='title-Homepage'>Buscar: </h4>
-                                    <input type="text" id='otherins' className="data-div-select" value={desglosesS}
-                                    style={{width: "58%"}} onChange={(e) => setDesglosesS(e.target.value)}/>
-                                </div>
-                                <select defaultValue={""}
-                                value={desSel} onChange={(e) => setDesSel(parseInt(e.target.value))} className='select-small-cco'>
-                                    <option value={""}>---</option>
-                                    {
-                                        desglosesF.map((lg,i) => (<option key={i} value={lg.desglose_id}>{lg.descripcion}</option>))
-                                    }
-                                </select>
-                                <button className='btn-export-pdf' onClick={() => addEnvio()}>Agregar</button>
-                            </div>
-                            }
-                            <div>
-                                {global && newEnvios.map((en, i) =>(
-                                    <div key={i} style={{border: "1px solid", borderColor: "#3399ff"}}>
-                                        <div style={{display: "flex" ,justifyContent: "space-between", alignItems: "center"}}>
-                                            <h3 className='title-Homepage' style={{margin: 2}}>{desgloseReturner(en.desgloseId, global.desgloses)}</h3>
-                                            <button className='btn-export-pdf' style={{backgroundColor: "red", borderColor: "red"}} onClick={() => deleteEnvio(i)}>X</button>
-                                        </div>
-                                        <div style={{ display: "flex",flexDirection: "column"}}>
-                                            <h4 className='title-Homepage'>Insumo: </h4>
-                                            <select value={newEnvios[i].addprod.sel} name="remitodes" onChange={(e) => handlerSel( i,parseInt(e.target.value))}>
-                                                {insRacionamieto.insumos.map((ins, i) => (
-                                                    <option value={i}>{ins.des}</option>
-                                                ))}
-                                            </select>
-                                        </div>
-                                        <div style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
-                                            <div >
-                                                <h4 className='title-Homepage'>Cajas: </h4>
-                                                <input type="number" id='otherins' className="data-div-select" min={0} value={newEnvios[i].addprod.cajas}
-                                                style={{width: "45%"}} onChange={(e) => handlerCaja(i, parseInt(e.target.value))}/>
-                                            </div>
-                                            <div >
-                                                <h4 className='title-Homepage'>Bolsas: </h4>
-                                                <input type="number" id='otherins' className="data-div-select" min={0} value={newEnvios[i].addprod.bolsas}
-                                                style={{width: "45%"}} onChange={(e) => handlerBolsa(i, parseInt(e.target.value))}/>
-                                            </div>
-                                            <button className='btn-export-pdf' onClick={() => addInsumo(i)}>+</button>
-                                        </div>
-                                        <div>
-                                            <h5 className='filter-sub'>Presione en un insumo para eliminarlo.</h5>
-                                            <table style={{width: 330, alignItems: "center"}}>
-                                                <tbody>
-                                                    <tr>
-                                                        <th style={{borderWidth: 1, borderColor: "black", borderStyle: "solid", width: 260}}>Insumo</th>
-                                                        <th style={{borderWidth: 1, borderColor: "black", borderStyle: "solid", width: 20}}>Kilos</th>
-                                                        <th style={{borderWidth: 1, borderColor: "black", borderStyle: "solid", width: 20}}>Cajas</th>
-                                                        <th style={{borderWidth: 1, borderColor: "black", borderStyle: "solid", width: 20}}>Bolsas</th>
-                                                        <th style={{borderWidth: 1, borderColor: "black", borderStyle: "solid", width: 20}}>Rac</th>
-                                                    </tr>
-                                                    {en.productos.map((ins,i2) => (
-                                                        <tr onClick={() => deleteProducto(i,i2)}>
-                                                            <th style={{borderWidth: 1, borderColor: "black", borderStyle: "solid", width: 260}}>{ins.des}</th>
-                                                            <th style={{borderWidth: 1, borderColor: "black", borderStyle: "solid", width: 20}}>{ins.kg}</th>
-                                                            <th style={{borderWidth: 1, borderColor: "black", borderStyle: "solid", width: 20}}>{ins.cajas}</th>
-                                                            <th style={{borderWidth: 1, borderColor: "black", borderStyle: "solid", width: 20}}>{ins.bolsas}</th>
-                                                            <th style={{borderWidth: 1, borderColor: "black", borderStyle: "solid", width: 20}}>{ins.rac}</th>
-                                                        </tr>
-                                                    ))}
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                        
-                                    </div>
-                                    
-                                ))}
-                            </div>
-                            {newEnvios.length > 0 && <button className='btn-big' onClick={() => createEnvios()}>Generar</button>}
-                        </div>
+                        <h4 className='title-Homepage'>Seleccione la tanda a eliminar</h4>
+                        <input type="number" id='otherins' className="data-div-select" value={tanda} min={1}
+                        style={{width: "35%"}} onChange={(e) => setTanda((e.target.value) ? parseInt(e.target.value) : 0)}/>
+                    </div>
+                    <div>
+                    <div>
+                        <h4 className='title-Homepage'>Escriba la clave</h4>
+                        <input type="text" id='otherins' className="data-div-select" value={delkey}
+                        style={{width: "35%"}} onChange={(e) => setDelkey(e.target.value)}/>
+                    </div>
+                    </div>
+                    <div>
+                        {tanda > 0 && <button className='btn-big' onClick={() => deleteEnvio()}>Eliminar</button>}
                     </div>
                 </div>
             </div>
         )
     }
-
     const displayTraerEnvios = () => {
 
         const getEnvios = async () => {
@@ -532,12 +351,13 @@ export default function Envios () {
                         onChange={(e)=>setDisplay(parseInt(e.target.value))}>
                             <option value={0}>---</option>
                             <option value={1}>Generar desglosado de envio</option>
+                            <option value={2}>Eliminar envios</option>
                             <option value={3}>Traer envios</option>
                         </select>
                     </div>
                     <div style={{maxWidth: 400}}>
                         {display === 1 && displayDesglosadoGn()}
-                        {display === 2 && displayNewEnvio()}
+                        {display === 2 && displayDeleteTanda()}
                         {display === 3 && displayTraerEnvios()}
                     </div>
                 </div>
